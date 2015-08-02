416167468a1c861c7910647cafc95ebdece67fef
rainvan <rainvan@gmail.com>
2015-08-02 22:07:01 +0800

    fix onestep.sh bug

--- OneStep.sh
+++ OneStep.sh
@@ -12,6 +12,19 @@ mkdir -p $V8DIR/bin;
 mkdir -p $V8DIR/lib;
 mkdir -p $V8DIR/include;
 
+## copy platform library
+if [ -f "out/x64.release/obj.target/tools/gyp/libv8_libplatform.a" ]
+then
+  cp out/x64.release/obj.target/tools/gyp/libv8_libplatform.a $V8DIR/lib/;
+elif [ -f "out/x64.release/libv8_libplatform.a" ]
+then
+  cp out/x64.release/libv8_libplatform.a $V8DIR/lib/;
+else
+  echo "The v8 platform static library not found!!!";
+  exit 1;
+fi
+
+## copy libv8.so.VERSION
 if [ -f "out/x64.release/lib.target/libv8.so.$V8VER" ]
 then
   cp out/x64.release/lib.target/libv8.so.$V8VER $V8DIR/lib/;
@@ -22,6 +35,9 @@ else
   echo "The v8 shared library not found!!!";
   exit 1;
 fi
+ln -sr $V8DIR/lib/libv8.so.$V8VER $V8DIR/lib/libv8.so;
+
+## others
 cp out/x64.release/d8 $V8DIR/bin/;
 cp out/x64.release/mksnapshot $V8DIR/bin/;
 cp out/x64.release/process $V8DIR/bin/;
@@ -29,5 +45,5 @@ cp out/x64.release/shell $V8DIR/bin/;
 cp include/v8*.h $V8DIR/include/;
 cp -r include/libplatform $V8DIR/include/;
 rm -f $V8DIR.tbz;
-tar -jvcf $V8DIR.tbz $V8DIR;
+tar --exclude=.* -jvcf $V8DIR.tbz $V8DIR;
 rm -rf $V8DIR;
