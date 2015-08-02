613d0aa28b444f12b35bba207082ba839f79d2d2
rainvan <rainvan@gmail.com>
2015-08-02 13:05:17 +0800

    add OneStep.sh build script

---
+++ OneStep.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+V8VER=4.4.63.25;
+V8DIR=v8-dev-$V8VER;
+CC=clang;
+CXX=clang++;
+LINK=clang++;
+make x64.release library=shared soname_version=$V8VER i18nsupport=off &&
+mkdir -p $V8DIR/lib;
+mkdir -p $V8DIR/include;
+cp out/x64.release/lib.target/libv8.so.$V8VER $V8DIR/lib/;
+cp include/v8*.h $V8DIR/include/;
+cp -r include/libplatform $V8DIR/include/;
+tar -jvcf $V8DIR.tbz $V8DIR;
+rm -rf $V8DIR;
